d24c950c1654620ae0869ca04ef6e4c53af0e4f6
GurleenKSahota <gsahota@seattleu.edu>
2026-02-01 19:00:34 -0800

    Add SSL support for database population script

--- customer-website/server/database/populate.js
+++ customer-website/server/database/populate.js
@@ -10,6 +10,7 @@ const pool = new Pool({
   database: process.env.DB_NAME || 'customer_website',
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
+  ssl: process.env.DB_HOST ? { rejectUnauthorized: false } : false,
 });
 
 async function runSchema() {
